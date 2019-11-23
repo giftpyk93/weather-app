a3fea2c5229986af76be690ac1a42679cbbf48e3
piyakorn jansiripitikul <giftzchan@gmail.com>
2019-11-23 22:42:38 +0700

    enhance getWeatherLocationFromLatLng function to get hourly weather data

--- src/Services/weather.tsx
+++ src/Services/weather.tsx
@@ -6,9 +6,14 @@ type LatLngType = { lat: number; lng: number };
 
 const getWeatherLocationFromLatLng = async ({ lat, lng }: LatLngType) => {
   const currentWeatherUrlPath = `${config.OPEN_WEATHER_BASE_API}/weather?lat=${lat}&lon=${lng}&units=metric&appid=${config.OPEN_WEATHER_API_KEY}`;
-  const currentWeather = await axios.get(currentWeatherUrlPath);
+  const hourlyWeatherUrlPath = `${config.OPEN_WEATHER_BASE_API}/forecast?lat=${lat}&lon=${lng}&units=metric&appid=${config.OPEN_WEATHER_API_KEY}`;
 
-  return currentWeather;
+  const [currentWeather, hourlyWeather] = await Promise.all([
+    axios.get(currentWeatherUrlPath),
+    axios.get(hourlyWeatherUrlPath),
+  ]);
+
+  return { currentWeather: currentWeather.data, hourlyWeather: hourlyWeather.data.list };
 };
 
 export { getWeatherLocationFromLatLng };
