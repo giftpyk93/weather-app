8a4fd8cca347f6f2bc41ad8135982e3d407bd0ca
piyakorn jansiripitikul <giftzchan@gmail.com>
2019-11-24 00:59:37 +0700

    fix grid

--- src/Components/CurrentWeatherDetail/index.tsx
+++ src/Components/CurrentWeatherDetail/index.tsx
@@ -2,24 +2,24 @@ import React, { FC } from "react";
 import Grid from "@material-ui/core/Grid";
 
 type WeatherDetailType = {
-    label: string;
-    value: string;
-}
+  label: string;
+  value: string;
+};
 
 type CurrentWeatherDetailPropsType = {
-    weatherDetails: WeatherDetailType[]
+  weatherDetails: WeatherDetailType[];
 };
 
 const CurrentWeatherDetail: FC<CurrentWeatherDetailPropsType> = ({ weatherDetails }) => {
   return (
-    <Grid container className="d-flex flex-column p-3">
+    <Grid container direction="column" className="p-3">
       <Grid item>
         <h6>CURRENT DETAIL</h6>
       </Grid>
       {weatherDetails.map((weatherDetail: WeatherDetailType) => {
-          const {label, value} = weatherDetail
+        const { label, value } = weatherDetail;
         return (
-          <Grid key={label} item container xs={12} className="d-flex flex-row">
+          <Grid key={label} item container xs={12} direction="row">
             <Grid item xs={1} />
             <Grid item xs={5} className="pl-4">
               {label}
