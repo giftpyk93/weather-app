da99abb10389b22c0ca9ef902d452bdae231785c
piyakorn jansiripitikul <giftzchan@gmail.com>
2019-11-23 16:48:09 +0700

    fix config file

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const config = {
   MAPBOX_TOKEN: process.env.REACT_APP_MAPBOX_TOKEN,
-  MAPBOX_SEARCH_PLACES_API: process.env.REACT_APP_MAPBOX_SEARCH_PLACES_API,
+  MAPBOX_SEARCH_PLACES_API: 'https://api.mapbox.com/geocoding/v5/mapbox.places',
 };
 
 export default config;
