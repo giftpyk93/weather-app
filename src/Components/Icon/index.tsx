d4434a2db5f7b8620c22e1059858b48c3b9c3bb6
piyakorn jansiripitikul <giftzchan@gmail.com>
2019-11-23 20:16:08 +0700

    add prop size for icon component

--- src/Components/Icon/index.tsx
+++ src/Components/Icon/index.tsx
@@ -5,16 +5,24 @@ import config from "../../config";
 
 type IconWeatherPropsType = {
   iconCode: string;
+  size?: 'small' | 'big';
 };
 
-const IconWrapper = styled.img`
-  width: 48px;
-  height: 48px;
+const ICON_SIZE = {
+  small: "48px",
+  big: "60px",
+};
+
+const SMALL = 'small'
+
+const IconWrapper = styled.img<Pick<IconWeatherPropsType, 'size'>>`
+  width: ${({ size }) => ICON_SIZE[size || SMALL]};
+  height: ${({ size }) => ICON_SIZE[size || SMALL]};
   text-align: center;
   object-fit: cover;
 `;
 
-export const WeatherIcon: FC<IconWeatherPropsType> = ({ iconCode }) => {
+export const WeatherIcon: FC<IconWeatherPropsType> = ({ iconCode, size }) => {
   const iconSrc = `${config.OPEN_WEATHER_ICON_URL}/${iconCode}@2x.png`;
-  return <IconWrapper alt="weather-icon" src={iconSrc} />;
+  return <IconWrapper alt="weather-icon" src={iconSrc} size={size} />;
 };
