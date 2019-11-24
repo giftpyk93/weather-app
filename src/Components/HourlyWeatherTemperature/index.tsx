5c2afbfddb2dabac05aa2e24c8f2602255a2dda1
piyakorn jansiripitikul <giftzchan@gmail.com>
2019-11-24 17:01:16 +0700

    using grid to manage layout

--- src/Components/HourlyWeatherTemperature/index.tsx
+++ src/Components/HourlyWeatherTemperature/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import moment from "moment";
 import styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
 
 import { TIME_FORMAT } from "Constants/DateTimeFormat";
 import { HourlyWeatherType } from "Containers/WeatherLocation/types";
@@ -16,28 +17,32 @@ const HourlyWeatherList = styled.div`
   justify-content: space-between;
   padding: 8px 0;
   overflow-x: scroll;
-  width: 100%;
+  width: calc(100vw - 32px);
 `;
 
 const HourlyWeather: FC<HourlyWeatherPropsType> = ({ hourlyWeather }) => {
   return (
-    <div className="px-3">
-      <h6>3 HOURS FORCAST FOR 5 DAYS</h6>
-      <HourlyWeatherList>
-        {hourlyWeather.map(hourlyWeather => {
-          const time = moment(hourlyWeather.dt_txt).format(TIME_FORMAT);
-          const temp = hourlyWeather.main.temp + "°C";
-          const iconCode = hourlyWeather.weather[0].icon;
-          return (
-            <div key={hourlyWeather.dt} className="d-flex flex-column align-items-center mx-2">
-              <p>{time}</p>
-              <WeatherIcon iconCode={iconCode} />
-              <p>{temp}</p>
-            </div>
-          );
-        })}
-      </HourlyWeatherList>
-    </div>
+    <Grid container className="px-3">
+      <Grid item>
+        <h6>24 HOURS FORCAST</h6>
+      </Grid>
+      <Grid item>
+        <HourlyWeatherList>
+          {hourlyWeather.map(hourlyWeather => {
+            const time = moment(hourlyWeather.dt_txt).format(TIME_FORMAT);
+            const temp = hourlyWeather.main.temp + "°C";
+            const iconCode = hourlyWeather.weather[0].icon;
+            return (
+              <div key={hourlyWeather.dt} className="d-flex flex-column align-items-center mx-2">
+                <p>{time}</p>
+                <WeatherIcon iconCode={iconCode} />
+                <p>{temp}</p>
+              </div>
+            );
+          })}
+        </HourlyWeatherList>
+      </Grid>
+    </Grid>
   );
 };
 
